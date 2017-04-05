30350a2c0abea76d4ecb6a72beb2501a4005b6f1
Brocq18 <Arun.september@gmail.com>
2017-04-05 14:45:44 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -4,12 +4,11 @@ Created on 5 Apr 2017
 The following scrapes data from the BBC site on premiership 
 football. It outputs the team name and the current points total.
 '''
-import urllib2
-from bs4 import BeautifulSoup
+import urllib2 # allows access to url links
+from bs4 import BeautifulSoup # used to scrape data from webpages
 
-soup = BeautifulSoup(urllib2.urlopen("http://www.bbc.co.uk/sport/football/tables", "lxml").read())
+soup = BeautifulSoup(urllib2.urlopen("http://www.bbc.co.uk/sport/football/tables", "lxml").read()) # reads target url
 
-for row in soup('table', {'class': 'table-stats'})[0].tbody('tr'):
+for row in soup('table', {'class': 'table-stats'})[0].tbody('tr'): # finds the table with class = 'table-stats' and extracts the information contained in the body(tbody) and tags(tr)
     tds = row('td')
-    print tds[2].string, tds[10].string # displays team and total points
-    
+    print tds[2].string, tds[10].string # displays team and total points 
